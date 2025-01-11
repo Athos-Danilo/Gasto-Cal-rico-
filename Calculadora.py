815d44c5b81992caeca46733dc463406774ae64b
Athos <athos-danilo@outlook.com>
2025-01-11 10:31:02 -0300

    Versão 3.0

--- Calculadora.py
+++ Calculadora.py
@@ -33,6 +33,7 @@ print("[3] Moderadamente ativo (Exercício moderado 3-5 dias por semana)")
 print("[4] Muito ativo (Exercício intenso 6-7 dias por semana)") 
 print("[5] Extremamente ativo (Atividade física muito intensa, trabalho físico pesado)") 
 nivel = int(input("Resposta: "))
+print("------------------------------------------------------------")
 while nivel != 1 and nivel != 2 and nivel != 3 and nivel != 4 and nivel != 5:
     print("||||||||||||||||||||||||||||||||||")
     print("Erro de Digitação!")
@@ -44,7 +45,6 @@ while nivel != 1 and nivel != 2 and nivel != 3 and nivel != 4 and nivel != 5:
     print("[5] Extremamente ativo (Atividade física muito intensa, trabalho físico pesado)") 
     nivel = int(input("Resposta: "))
     print("------------------------------------------------------------")
-
 
 if nivel == 1:
     calculo_tmt = calculo_tmb * 1.2 
@@ -61,3 +61,151 @@ elif nivel == 4:
 elif nivel == 5:
     calculo_tmt = calculo_tmb * 1.9 
     print(f"{nome}, sua Taxa de Metabolismo Total é de {calculo_tmt:.2f} calórias.")
+
+print("------------------------------------------------------------")
+print("Você gostaria de [1] Perder Peso ou [2] Ganhar Peso ?")
+objetivo = int(input("Resposta: "))
+while objetivo != 1 and objetivo != 2:
+    print("||||||||||||||||||||||||||||||||||")
+    print("Erro de Digitação!")
+    print("Você gostaria de [1] Perder Peso ou [2] Ganhar Peso ?")
+    objetivo = int(input("Resposta: "))
+
+if objetivo == 1:
+    print("Como gostaria de definir seu Déficit Calórico?")
+    print("[1] Déficit Leve")
+    print("[2] Déficit Moderado")
+    print("[3] Déficit Agressivo")
+    deficit = int(input("Resposta: "))
+    if deficit == 1:
+        print("------------------------------------------------------------")
+        print("Ótima Escolha! O Déficit Leve é ideal para quem busca uma perda lenta e sustentável de peso.")
+        print("Escolha a porcentagem do Déficit, entre 5 a 10%")
+        porcentagem = float(input("Resposta: "))
+        calculo_porcentagem = porcentagem/100
+        deficit_leve = calculo_tmt - (calculo_porcentagem*calculo_tmt)
+        print("------------------------------------------------------------")
+        print(f"{nome}, você irá precisar consumir {deficit_leve:.2f} Calórias para Perder Peso.")
+        print("------------------------------------------------------------")
+    elif deficit == 2:
+        print("------------------------------------------------------------")
+        print("Ótima Escolha! O Déficit Moderado é ideal para as pessoas que desejam emagrecer com segurança e certa velocidade.")
+        print("Escolha a porcentagem do Déficit, entre 10 a 20%")
+        porcentagem = float(input("Resposta: "))
+        calculo_porcentagem = porcentagem/100
+        deficit_moderado = calculo_tmt - (calculo_porcentagem*calculo_tmt)
+        print("------------------------------------------------------------")
+        print(f"{nome}, você irá precisar consumir {deficit_moderado:.2f} Calórias para Perder Peso.")
+        print("------------------------------------------------------------")
+    elif deficit == 3:
+        print("------------------------------------------------------------")
+        print("O Déficit Agressivo, é usado em casos específicos, como preparação de atletas ou sob supervisão médica.")
+        print("Escolha a porcentagem do Déficit, entre 20 a 30%")
+        porcentagem = float(input("Resposta: "))
+        calculo_porcentagem = porcentagem/100
+        deficit_agressivo = calculo_tmt - (calculo_porcentagem*calculo_tmt)
+        print("------------------------------------------------------------")
+        print(f"{nome}, você irá precisar consumir {deficit_agressivo:.2f} Calórias para Perder Peso.")
+        print("------------------------------------------------------------")
+if objetivo == 2:
+    print("Como gostaria de definir seu Ganho de Peso?")
+    print("[1] Superávit Leve")
+    print("[2] Superávit Moderado")
+    print("[3] Superávit Agressivo")
+    superavit = int(input("Resposta: "))
+    if superavit == 1:
+        print("------------------------------------------------------------")
+        print("Ótima Escolha! O Superávit Leve é ideal para quem busca ganho de peso gradual, com menor acúmulo de gordura.")
+        print("Escolha a porcentagem do Superávit, entre 5 a 10%")
+        porcentagem = float(input("Resposta: "))
+        calculo_porcentagem = porcentagem/100
+        superavit_leve = calculo_tmt + (calculo_porcentagem*calculo_tmt)
+        print("------------------------------------------------------------")
+        print(f"{nome}, você irá precisar consumir {superavit_leve:.2f} Calórias para Ganhar Peso.")
+        print("------------------------------------------------------------")
+    if superavit == 2:
+        print("------------------------------------------------------------")
+        print("Ótima Escolha! O Superávit Moderado é ideal para a maioria das pessoas que desejam ganhar peso ou aumentar a massa muscular.")
+        print("Escolha a porcentagem do Superávit, entre 10 a 20%")
+        porcentagem = float(input("Resposta: "))
+        calculo_porcentagem = porcentagem/100
+        superavit_moderado = calculo_tmt + (calculo_porcentagem*calculo_tmt)
+        print("------------------------------------------------------------")
+        print(f"{nome}, você irá precisar consumir {superavit_moderado:.2f} Calórias para Ganhar Peso.")
+        print("------------------------------------------------------------")
+    if superavit == 3:
+        print("------------------------------------------------------------")
+        print("Usado em casos específicos, como pessoas com metabolismo extremamente acelerado ou dificuldades extremas de ganho de pes")
+        print("Escolha a porcentagem do Superávit, entre 20 a 30%")
+        porcentagem = float(input("Resposta: "))
+        calculo_porcentagem = porcentagem/100
+        superavit_agressivo = calculo_tmt + (calculo_porcentagem*calculo_tmt)
+        print("------------------------------------------------------------")
+        print(f"{nome}, você irá precisar consumir {superavit_agressivo:.2f} Calórias para Ganhar Peso.")
+        print("------------------------------------------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# QUANTIDADE DE MACRO NUTRIENTES!
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Olá Usuário!
 A ideia desse projeto é ajudar pessoas a perder ou ganhar peso.
+Em seguida, a aplicação calculará duas taxas metabólicas e, depois, perguntará ao usuário qual é o seu objetivo. Por fim, apresentará os valores necessários para auxiliar o usuário a alcançá-lo.
 
 TMB (Taxa de Metabolismo Basal) é a quantidade mínima de calorias que o corpo precisa para funcionar em repouso, como respirar e manter a temperatura.
 
